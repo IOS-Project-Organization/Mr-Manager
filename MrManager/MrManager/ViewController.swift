315ef3171405f36c1e3ae0c33b66751a8389b3d5
Dong-eun Shin <55118858+dongeunshin@users.noreply.github.com>
2022-06-09 20:06:38 +0900

    Add files via upload

---
+++ MrManager/MrManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MrManager
+//
+//  Created by dong eun shin on 2022/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
