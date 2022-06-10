f8869024bc28d8cab89393caf7a916cb6cce7189
KiHyunJang <73085636+KiHyunJang@users.noreply.github.com>
2022-06-10 23:01:31 +0900

    Project init
    폴더를 나눴는데 파일이 없으면 github에서 나타나지 않아서 빈 swift추가.

---
+++ MrManager/MrManager/Data/Repository/d.swift
@@ -0,0 +1,8 @@
+//
+//  d.swift
+//  MrManager
+//
+//  Created by Ki Hyun on 2022/06/10.
+//
+
+import Foundation
---
+++ MrManager/MrManager/Domain/UseCase/c.swift
@@ -0,0 +1,8 @@
+//
+//  c.swift
+//  MrManager
+//
+//  Created by Ki Hyun on 2022/06/10.
+//
+
+import Foundation
