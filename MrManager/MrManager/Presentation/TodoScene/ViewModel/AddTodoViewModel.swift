69957cdf57be923cb24eb6c2abda8db7b23bdae4
KiHyunJang <73085636+KiHyunJang@users.noreply.github.com>
2022-06-12 20:46:14 +0900

    [Feat] 프로젝트 폴더 및 파일 네이밍
    각자 맡은 UI 구성전에 프로젝트의 폴더와 파일 이름 통일을 위해서 수정.

---
+++ MrManager/MrManager/Presentation/TodoScene/ViewModel/AddTodoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddTodoViewModel.swift
+//  MrManager
+//
+//  Created by Ki Hyun on 2022/06/12.
+//
+
+import Foundation
---
+++ MrManager/MrManager/Presentation/TodoScene/ViewModel/TodoListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TodoListViewModel.swift
+//  MrManager
+//
+//  Created by Ki Hyun on 2022/06/12.
+//
+
+import Foundation
